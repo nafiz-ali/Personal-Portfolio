5fa45436502b726fa5ccf6bbd53beb178d59d7cc
SYED KHADAR  NAFIZ ALI BASHA <129795181+nafiz-ali@users.noreply.github.com>
2025-07-18 11:23:34 +0530

    adding base files!!!!

---
+++ types.ts
@@ -0,0 +1,19 @@
+
+import type { ReactNode } from 'react';
+
+export type Theme = 'dark' | 'light-dark';
+
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  tags: string[];
+  liveUrl?: string;
+  repoUrl?: string;
+}
+
+export interface Skill {
+  name: string;
+  icon: ReactNode;
+}
